a2ed4c5957d924938ffdea78f988aad1a527fe1a
Ashgash01 <lucaf8565@gmail.com>
2025-02-06 00:47:46 +0200

    Add
    Added new inv,
    new item system,
    Item dic,
    pickup ability.
    
    Missing save inv (inventorySaveData)

---
+++ Assets/Scripts/InventorySaveData.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+public class InventorySaveData
+{
+    public int itemID;
+    public int slotIndex;
+}
---
+++ Assets/Scripts/SaveController.cs
@@ -0,0 +1,45 @@
+using System.Collections.Generic;
+using System.IO;
+using Unity.Cinemachine;
+using UnityEngine;
+
+public class SaveController : MonoBehaviour
+{
+    private string saveLocation;
+    private InventoryController inventoryController;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        saveLocation = Path.Combine(Application.persistentDataPath, "saveData.json");
+        inventoryController = FindFirstObjectByType<InventoryController>();
+        LoadGame();
+    }
+
+    public void SaveGame()
+    {
+        SaveData saveData = new SaveData
+        {
+            playerPosition = GameObject.FindGameObjectWithTag("Player").transform.position,
+            inventorySaveData = inventoryController.GetInventoryItems() ?? new List<InventorySaveData>()
+        };
+
+        File.WriteAllText(saveLocation, JsonUtility.ToJson(saveData));
+    }
+
+    public void LoadGame()
+    {
+        if (File.Exists(saveLocation))
+        {
+            SaveData saveData = JsonUtility.FromJson<SaveData>(File.ReadAllText(saveLocation));
+
+            GameObject.FindGameObjectWithTag("Player").transform.position = saveData.playerPosition;
+
+            inventoryController.SetInventoryItems(saveData.inventorySaveData);
+        }
+        else
+        {
+            SaveGame();
+            inventoryController.SetInventoryItems(new List<InventorySaveData>());
+        }
+    }
+}
