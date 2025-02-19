1c61d71aa6223725dcfb4b92541cba43910f1a75
Ashgash01 <lucaf8565@gmail.com>
2025-02-19 20:53:53 +0200

    max health and min

--- Assets/HealthBar.cs
+++ Assets/HealthBar.cs
@@ -8,6 +8,7 @@ public class HealthBar : MonoBehaviour
     private float currentHealth;
     [SerializeField] private Image healthBarFill;
     [SerializeField] private TextMeshProUGUI healthText;
+    [SerializeField] private float fillSpeed;
     void Start()
     {
         currentHealth = maxHealth;
@@ -17,6 +18,7 @@ public class HealthBar : MonoBehaviour
     public void UpdateHealth(float amount)
     {
         currentHealth += amount;
+        currentHealth = Mathf.Clamp(currentHealth, 0f, maxHealth);
         healthText.text = "Health : " + currentHealth;
         UpdateHealthBar();
     }
